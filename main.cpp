9452719f1861df32ce968d268bd6592284605612
Gytis <gytis@vaicekauskas.lt>
2012-10-09 11:40:31 +0200

    Lalala
    Versija 2.0, remotu valdo naikintuvus

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    cout << "Hello world, ";
+    cout << "Hello world\n";
+    cout << "Bujaka bujaka ";
     system("PAUSE");
     return EXIT_SUCCESS;
 }
